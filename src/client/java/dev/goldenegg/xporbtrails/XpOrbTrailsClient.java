3f935450e0ecc77b6e2e6b406594213b6bd44767
GoldenEgg1324 <blackhole041324@gmail.com>
2026-08-31 21:26:45 -0500

    Use 26.1 extraction, render, and key-mapping events

--- src/client/java/dev/goldenegg/xporbtrails/XpOrbTrailsClient.java
+++ src/client/java/dev/goldenegg/xporbtrails/XpOrbTrailsClient.java
@@ -5,9 +5,10 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParseException;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
-import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.level.LevelExtractionEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.resources.Identifier;
@@ -49,12 +50,13 @@ public final class XpOrbTrailsClient implements ClientModInitializer {
             saveConfig();
         }
 
-        WorldRenderEvents.AFTER_ENTITIES.register(TrailRenderer::render);
+        LevelExtractionEvents.END_EXTRACTION.register(TrailRenderer::extract);
+        LevelRenderEvents.AFTER_TRANSLUCENT_TERRAIN.register(TrailRenderer::render);
 
-        KeyMapping.Category settingsCategory = KeyMapping.Category.register(
+        KeyMapping.Category category = KeyMapping.Category.register(
                 Identifier.fromNamespaceAndPath("xporbtrails", "settings"));
-        KeyMapping openSettings = KeyBindingHelper.registerKeyBinding(new KeyMapping(
-                "key.xporbtrails.open_settings", InputConstants.UNKNOWN.getValue(), settingsCategory));
+        KeyMapping openSettings = KeyMappingHelper.registerKeyMapping(new KeyMapping(
+                "key.xporbtrails.open_settings", InputConstants.UNKNOWN.getValue(), category));
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openSettings.consumeClick()) client.setScreen(createConfigScreen(client.screen));
         });
